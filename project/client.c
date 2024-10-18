68bf0dfabfc44d0792f9f13df6c4d86d726a827f
henrynvn09 <henrynvn09@gmail.com>
2024-10-17 20:32:58 -0700

    done project 0 cs 118

--- project/client.c
+++ project/client.c
@@ -3,47 +3,81 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
-int main() {
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+        return 1;
+
+    char *hostname = argv[1];
+    if (strcmp("localhost", hostname) == 0)
+        hostname = "127.0.0.1";
+
+    int port = atoi(argv[2]);
+
     /* 1. Create socket */
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-                     // use IPv4  use UDP
+    // use IPv4  use UDP
 
     /* 2. Construct server address */
     struct sockaddr_in serveraddr;
     serveraddr.sin_family = AF_INET; // use IPv4
-    serveraddr.sin_addr.s_addr = INADDR_ANY;
+    serveraddr.sin_addr.s_addr = inet_addr(hostname);
     // Set sending port
-    int SEND_PORT = 8080;
-    serveraddr.sin_port = htons(SEND_PORT); // Big endian
+    // int SEND_PORT = 8080;
+    serveraddr.sin_port = htons(port); // Big endian
 
+    int BUF_SIZE = 1024;
     /* 3. Send data to server */
-    char client_buf[] = "Hello world!";
-    int did_send = sendto(sockfd, client_buf, strlen(client_buf), 
-                       // socket  send data   how much to send
-                          0, (struct sockaddr*) &serveraddr, 
-                       // flags   where to send
-                          sizeof(serveraddr));
-    if (did_send < 0) return errno;
+    char client_buf[BUF_SIZE];
+    // int did_send = sendto(sockfd, client_buf, strlen(client_buf),
+    //                    // socket  send data   how much to send
+    //                       0, (struct sockaddr*) &serveraddr,
+    //                    // flags   where to send
+    //                       sizeof(serveraddr));
+    // if (did_send < 0) return errno;
 
     /* 4. Create buffer to store incoming data */
-    int BUF_SIZE = 1024;
     char server_buf[BUF_SIZE];
     socklen_t serversize = sizeof(socklen_t); // Temp buffer for recvfrom API
 
-    /* 5. Listen for response from server */
-    int bytes_recvd = recvfrom(sockfd, server_buf, BUF_SIZE, 
-                            // socket  store data  how much
-                               0, (struct sockaddr*) &serveraddr, 
-                               &serversize);
-    // Execution will stop here until `BUF_SIZE` is read or termination/error
-    // Error if bytes_recvd < 0 :(
-    if (bytes_recvd < 0) return errno;
-    // Print out data
-    write(1, server_buf, bytes_recvd);
+    // socket non-blocking
+    int out_flags = fcntl(sockfd, F_GETFL) | O_NONBLOCK;
+    fcntl(sockfd, F_SETFL, out_flags);
+    // standard input non-blocking
+    int inp_flags = fcntl(0, F_GETFL) | O_NONBLOCK;
+    fcntl(sockfd, F_SETFL, inp_flags);
 
+    while (1)
+    {
+        /* 5. Listen for response from server */
+        int bytes_recvd = recvfrom(sockfd, server_buf, BUF_SIZE,
+                                   // socket  store data  how much
+                                   0, (struct sockaddr *)&serveraddr,
+                                   &serversize);
+        // if data has been received
+        if (bytes_recvd > 0)
+        {
+            write(1, server_buf, bytes_recvd);
+        }
 
-    /* 6. You're done! Terminate the connection */     
+        int inp_size = read(0, client_buf, BUF_SIZE);
+        /* 3. send request to the server if receive input*/
+        if (inp_size > 0)
+        {
+            int did_send = sendto(sockfd, client_buf, inp_size,
+                                  // socket  send data   how much to send
+                                  0, (struct sockaddr *)&serveraddr,
+                                  // flags   where to send
+                                  sizeof(serveraddr));
+            if (did_send < 0)
+                return errno;
+        }
+    }
+
+    /* 6. You're done! Terminate the connection */
     close(sockfd);
     return 0;
 }
--- project/server.c
+++ project/server.c
@@ -3,61 +3,88 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
-int main() {
-    /* 1. Create socket */
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-                     // use IPv4  use UDP
+int main(int argc, char **argv)
+{
 
-    /* 2. Construct our address */
-    struct sockaddr_in servaddr;
-    servaddr.sin_family = AF_INET; // use IPv4
-    servaddr.sin_addr.s_addr = INADDR_ANY; // accept all connections
-                            // same as inet_addr("0.0.0.0") 
-                                     // "Address string to network bytes"
-    // Set receiving port
-    int PORT = 8080;
-    servaddr.sin_port = htons(PORT); // Big endian
+   if (argc != 2)
+      return 1;
 
-    /* 3. Let operating system know about our config */
-    int did_bind = bind(sockfd, (struct sockaddr*) &servaddr, 
-                        sizeof(servaddr));
-    // Error if did_bind < 0 :(
-    if (did_bind < 0) return errno;
+   int PORT = atoi(argv[1]);
 
-    /* 4. Create buffer to store incoming data */
-    int BUF_SIZE = 1024;
-    char client_buf[BUF_SIZE];
-    struct sockaddr_in clientaddr; // Same information, but about client
-    socklen_t clientsize = sizeof(clientaddr);
+   /* 1. Create socket */
+   int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+   // use IPv4  use UDP
 
-    /* 5. Listen for data from clients */
-    int bytes_recvd = recvfrom(sockfd, client_buf, BUF_SIZE, 
-                            // socket  store data  how much
-                               0, (struct sockaddr*) &clientaddr, 
-                               &clientsize);
-    // Execution will stop here until `BUF_SIZE` is read or termination/error
-    // Error if bytes_recvd < 0 :(
-    if (bytes_recvd < 0) return errno;
+   /* 2. Construct our address */
+   struct sockaddr_in servaddr;
+   servaddr.sin_family = AF_INET;         // use IPv4
+   servaddr.sin_addr.s_addr = INADDR_ANY; // accept all connections
+                                          // same as inet_addr("0.0.0.0")
+                                          // "Address string to network bytes"
+   // Set receiving port
+   //  int PORT = 8080;
+   servaddr.sin_port = htons(PORT); // Big endian
 
-    /* 6. Inspect data from client */
-    char* client_ip = inet_ntoa(clientaddr.sin_addr);
-                    // "Network bytes to address string"
-    int client_port = ntohs(clientaddr.sin_port); // Little endian
-    // Print out data
-    write(1, client_buf, bytes_recvd);
+   /* 3. Let operating system know about our config */
+   int did_bind = bind(sockfd, (struct sockaddr *)&servaddr,
+                       sizeof(servaddr));
+   // Error if did_bind < 0 :(
+   if (did_bind < 0)
+      return errno;
 
-    /* 7. Send data back to client */
-    char server_buf[] = "Hello world!";
-    int did_send = sendto(sockfd, server_buf, strlen(server_buf), 
-                       // socket  send data   how much to send
-                          0, (struct sockaddr*) &clientaddr, 
-                       // flags   where to send
-                          sizeof(clientaddr));
-    if (did_send < 0) return errno;
-    
+   /* 4. Create buffer to store incoming data */
+   int BUF_SIZE = 1024;
+   char client_buf[BUF_SIZE];
+   char server_buf[BUF_SIZE];
+   struct sockaddr_in clientaddr; // Same information, but about client
+   socklen_t clientsize = sizeof(clientaddr);
 
-    /* 8. You're done! Terminate the connection */     
-    close(sockfd);
-    return 0;
+   // socket non-blocking
+   int out_flags = fcntl(sockfd, F_GETFL) | O_NONBLOCK;
+   fcntl(sockfd, F_SETFL, out_flags);
+   // standard input non-blocking
+   int inp_flags = fcntl(0, F_GETFL) | O_NONBLOCK;
+   fcntl(sockfd, F_SETFL, inp_flags);
+
+   while (1)
+   {
+      /* 5. Listen for data from clients */
+      int bytes_recvd = recvfrom(sockfd, client_buf, BUF_SIZE,
+                                 // socket  store data  how much
+                                 0, (struct sockaddr *)&clientaddr,
+                                 &clientsize);
+      // Execution will stop here until `BUF_SIZE` is read or termination/error
+      // Error if bytes_recvd < 0 :(
+      // if no data has been received and client has not connected:
+      if (bytes_recvd < 0)
+         continue;
+
+      // Now, the client has connected (data has been sent at some point)
+      /* 6. Inspect data from client */
+      char *client_ip = inet_ntoa(clientaddr.sin_addr);
+      // "Network bytes to address string"
+      int client_port = ntohs(clientaddr.sin_port); // Little endian
+      // Print out data
+      write(1, client_buf, bytes_recvd);
+
+      int inp_size = read(0, server_buf, BUF_SIZE);
+      /* 7. Send data back to client */
+      if (inp_size > 0)
+      {
+         int did_send = sendto(sockfd, server_buf, inp_size,
+                               // socket  send data   how much to send
+                               0, (struct sockaddr *)&clientaddr,
+                               // flags   where to send
+                               sizeof(clientaddr));
+         if (did_send < 0)
+            return errno;
+      }
+   }
+
+   /* 8. You're done! Terminate the connection */
+   close(sockfd);
+   return 0;
 }
